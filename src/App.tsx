4bba3e012fba828fc9d6a8d3771d2356b56f4ea3
Silvan Anggia Bayu <silvan.anggia@lapan.go.id>
2021-10-30 18:38:51 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react";
-import Navbar from "./components/navbar";
 import Header from "./components/header";
-import About from "./components/about";
-import Faq from "./components/faq";
-import Contact from "./components/contact";
-import Roadmap from "./components/roadmap";
 import "./App.css";
 
 const App = () => {
